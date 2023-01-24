e2784d6dd4ea49aff07e8bb7b1fe714298cb4962
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-01-23 21:25:53 -0300

    fix error onn override flatpak apps

--- after-install/flatpak.sh
+++ after-install/flatpak.sh
@@ -27,6 +27,7 @@ for app in ${APPS[@]}; do
   if [[ ! " ${NOT_OVERRIDE_APPS[*]} " == *" $app "* ]]; then
     echo $'Overriding' "$app..."
     sudo flatpak override --filesystem=host "$app"
+    sudo flatpak override --filesystem=xdg-config/gtk-4.0 "$app"
   fi
 done
 echo $'\nAll override completed\n'
