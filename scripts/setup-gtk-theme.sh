c1901bf579212a33b0c0c0dc0724ce46319c1e1c
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-06-30 22:39:55 -0300

    feat: use Colloid as gtk theme

--- scripts/setup-gtk-theme.sh
+++ scripts/setup-gtk-theme.sh
@@ -3,23 +3,23 @@
 echo "Setting up GTK Theme..."
 
 echo "Removing old files if exists..."
-sudo rm -rf /usr/share/themes/Catppuccin-Mocha-Standard-Blue-*
-rm -rf ~/.themes/Catppuccin-Mocha-Standard-Blue-*
+sudo rm -rf /usr/share/themes/Colloid-*
+rm -rf ~/.themes/Colloid-*
 rm -rf /tmp/gtk-theme
 rm -rf ~/.config/gtk-4.0
 
 echo "Cloning GTK Theme..."
-git clone --recurse-submodules https://github.com/catppuccin/gtk.git /tmp/gtk-theme
+git clone https://github.com/vinceliuice/Colloid-gtk-theme.git /tmp/gtk-theme
 
 echo "Installing GTK Theme..."
-bash -c "cd /tmp/gtk-theme && virtualenv -p python3 venv && source venv/bin/activate && pip install -r requirements.txt && python install.py mocha -a blue -s standard -l --tweaks rimless"
-sudo mv ~/.themes/Catppuccin-Mocha-Standard-Blue-* /usr/share/themes
+bash -c "cd /tmp/gtk-theme && ./install.sh -c dark -s standard -l fixed --tweaks catppuccin black rimless"
+sudo mv ~/.themes/Colloid-* /usr/share/themes
 
 echo "Linking GTK-4.0 Theme..."
 mkdir -p ~/.config/gtk-4.0
-ln -fs /usr/share/themes/Catppuccin-Mocha-Standard-Blue-Dark/gtk-4.0/assets ~/.config/gtk-4.0/assets
-ln -fs /usr/share/themes/Catppuccin-Mocha-Standard-Blue-Dark/gtk-4.0/gtk.css ~/.config/gtk-4.0/gtk.css
-ln -fs /usr/share/themes/Catppuccin-Mocha-Standard-Blue-Dark/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk-dark.css
+ln -fs /usr/share/themes/Colloid-Dark-Catppuccin/gtk-4.0/assets ~/.config/gtk-4.0/assets
+ln -fs /usr/share/themes/Colloid-Dark-Catppuccin/gtk-4.0/gtk.css ~/.config/gtk-4.0/gtk.css
+ln -fs /usr/share/themes/Colloid-Dark-Catppuccin/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk-dark.css
 
 echo "Defining GTK Theme..."
 gsettings set org.gnome.desktop.interface gtk-theme "Catppuccin-Mocha-Standard-Blue-Dark"
