9350690d7414082d8891cc716e5153261647954e
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-03-27 19:05:17 -0300

    chore: update neovim install script

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -14,8 +14,8 @@ git clone --depth 1 -b "$BRANCH" "$GIT_URL" "$WORK_DIR"
 
 # Install
 echo "Installing..."
-cd "$WORK_DIR"
+cd "$WORK_DIR" || exit
 make CMAKE_BUILD_TYPE=RelWithDebInfo
-cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb && cd "$WORK_DIR"
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64-x86_64.deb && cd "$WORK_DIR" || exit
 
 echo "Neovim installed..."
